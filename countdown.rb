14ba8e81c258da5ac0e56f065fcfb9dc696245a0
James Lotruglio <jimlotruglio@yahoo.com>
2018-02-13 00:29:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,15 @@ def countdown(number)
     number -= 1
   end
      "HAPPY NEW YEAR!"
+end
 
 
+def countdown_with_sleep(number)
+
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep 5
+    number -= 1
+  end
+     "HAPPY NEW YEAR!"
 end
